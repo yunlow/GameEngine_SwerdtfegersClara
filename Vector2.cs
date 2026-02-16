10dfaca5df5cba15aea9f010a56d2a41c9d0f7dd
yunlow <swerdtfegerslucas@gmail.com>
2026-02-16 13:59:53 +0100

    Classe Vector2
    ajout des deux champs et leur getter/setter

---
+++ Vector2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace GameEngine_SwerdtfegersLucas
+{
+    internal class Vector2
+    {
+        private float _x;
+        private float _y;
+
+        public float GetX() { return _x; } 
+        public void SetX(float x) { _x = x; }
+        public float GetY() { return _y; }
+        public void SetY(float y) { _y = y; }
+                        
+        
+    }
+}
