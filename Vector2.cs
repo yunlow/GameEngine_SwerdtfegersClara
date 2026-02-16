776bd8f69ad8b3afe4367b9c91e7f1a035846b20
yunlow <swerdtfegerslucas@gmail.com>
2026-02-16 23:21:32 +0100

    Vector2 constructeur

--- Vector2.cs
+++ Vector2.cs
@@ -10,6 +10,12 @@ namespace GameEngine_SwerdtfegersLucas
         private float _x;
         private float _y;
 
+        public Vector2(float x, float y)
+        {
+            _x = x;
+            _y = y;
+        }
+
         public float GetX() { return _x; } 
         public void SetX(float x) { _x = x; }
         public float GetY() { return _y; }
