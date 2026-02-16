f69b317c76c6d4d370a99ba61d9a9ad636dde5fe
yunlow <swerdtfegerslucas@gmail.com>
2026-02-16 09:53:13 +0100

    Ajout de la boucle realtime

--- GameEngine.cs
+++ GameEngine.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace GameEngine_SwerdtfegersLucas
 {
     internal class GameEngine
     {
+        private bool _shouldQuit = false;
+        private readonly Stopwatch _stopwatch = new Stopwatch();
         public void Run()
         {
-            ProcessInput();
-            Update();
-            Render();
+            float last_time = GetCurrentTime();
+            while (!_shouldQuit)
+            {
+                float loop_start_time = GetCurrentTime();
+                float elapsed_time = loop_start_time - last_time;
+                ProcessInput();
+                Update(elapsed_time); // ICI
+                Render();
+                last_time = loop_start_time;
+
+            }
         }
 
         public void ProcessInput()
@@ -18,7 +29,7 @@ namespace GameEngine_SwerdtfegersLucas
            
         }
 
-        public void Update()
+        public void Update(float elapsed_time)
         {
 
         }
@@ -26,6 +37,11 @@ namespace GameEngine_SwerdtfegersLucas
         public void Render()
         {
 
+        }
+
+        public float GetCurrentTime()
+        {
+            return _stopwatch.ElapsedMilliseconds / 1000.0f;
         }
     }
 }
