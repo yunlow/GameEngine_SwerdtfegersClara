49b281dc2a4bd19dc7c999e6cab97438ad0d4e2f
yunlow <swerdtfegerslucas@gmail.com>
2026-02-16 23:24:59 +0100

    Player Setters
    ajout des setters et d'un truc pour avoir la speed

--- Player.cs
+++ Player.cs
@@ -7,9 +7,8 @@ namespace GameEngine_SwerdtfegersLucas
     public class Player
     {
         private Vector2 _position = new Vector2(5, 5);
-;
         private Vector2 _direction = new Vector2(0, 0);
-;
+
         private string _renderGraphic = "@";
         private float speed = 10f;
 
@@ -22,14 +21,34 @@ namespace GameEngine_SwerdtfegersLucas
 
         public void SetDirection(Vector2 new_direction)
         {
-
+            _direction = new_direction;
         }
 
-        
+        public Vector2 GetDirection()
+
+        {
+            return _direction;
+    }
+
+
 
         public Vector2 GetPosition()
         {
             return _position;
         }
+
+        public void SetPosition(Vector2 new_position)
+
+        {
+         _position = new_position;
+         }
+
+        public float GetSpeed()
+
+        {
+           return speed;
+        }
+
+
     }
 }
