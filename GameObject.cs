47bd91d3afd746f6e1bcff95fc9af35bdc4f3290
yunlow <swerdtfegerslucas@gmail.com>
2026-03-03 13:06:52 +0100

    ajout de gameobject

---
+++ GameObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameEngine_SwerdtfegersLucas
+{
+    public abstract class GameObject
+    {
+        public abstract void Update(float elapsed_time);
+        public abstract void FixedUpdate(float fixed_elapsed_time);
+    }
+}
