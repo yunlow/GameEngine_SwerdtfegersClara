b90b6cef7d31c113c551faf3328fc36e84759a6f
yunlow <swerdtfegerslucas@gmail.com>
2026-03-19 14:02:11 +0100

    statemachine

---
+++ StateMachine.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameEngine_SwerdtfegersLucas
+{
+    public class StateMachine
+    {
+        private IState _currentState;
+        public void ChangeState(IState new_state)
+        {
+            if (_currentState != null)
+            {
+                _currentState.Exit();
+            }
+            _currentState = new_state;
+            _currentState.Enter();
+        }
+
+        public void Update(float elapsed_time)
+        {
+            if (_currentState != null)
+            {
+                _currentState.Update(elapsed_time);
+            }
+        }
+
+        public void FixedUpdate(float fixed_elapsed_time)
+        {
+            if (_currentState != null)
+            {
+                _currentState.FixedUpdate(fixed_elapsed_time);
+            }
+        }
+
+        public void ProcessInput(ConsoleKeyInfo input)
+        {
+            if (_currentState != null)
+            {
+                _currentState.ProcessInput(input);
+            }
+        }
+        public void Render()
+        {
+            if (_currentState != null)
+            {
+                _currentState.Render();
+            }
+        }
+        public void SetInitialState(IState initial_state)
+        {
+            _currentState = initial_state;
+            _currentState.Enter();
+        }
+
+    }
+}
